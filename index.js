d8470b9f22ec11d9fe01c4a503d79d5685e20c1a
Liam Oehlman <liam.oehlman@gmail.com>
2015-01-28 16:30:44 +1000

    Initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+var AWS = require('aws-sdk');
+
+AWS.config.loadFromPath('./config.json');
+
+var cloudwatchlogs = new AWS.CloudWatchLogs();
+
+var initialParams = {
+  logGroupName: process.argv[2], /* required */
+  logStreamName: process.argv[3], /* required */
+  startFromHead: false
+};
+
+initialParams.startTime = new Date().getTime()-360;
+
+function getLogs(params) {
+    params.endTime = new Date().getTime()-360;
+
+    cloudwatchlogs.getLogEvents(params, function(error, data) {
+        if (error) {
+            console.log(error)
+        }
+
+        if (data.events.length !== 0) {
+            data.events.forEach(function(event) {
+                console.log(new Date(event.timestamp) + ' : ' + event.message);
+            });
+        }
+
+        params.startTime = params.endTime,
+
+        params.nextToken = data.nextForwardToken;
+
+        setTimeout(getLogs, 2000, params)
+    })
+}
+
+setTimeout(getLogs, 2000, initialParams);
